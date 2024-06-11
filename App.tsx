9a68cba8ce6caa204ba94ff51b4f5cf34dbd6a5d
hoang22032004 <130357100+hoang22032004@users.noreply.github.com>
2024-06-11 19:58:55 +0700

    hihi

--- App.tsx
+++ App.tsx
@@ -8,6 +8,8 @@ import Setting from 'containers/auth/Setting'
 import Login from 'containers/auth/Login'
 import CameraApp from 'components/CameraApp'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import Welcome01 from 'containers/auth/welcome/Welcome01'
+import Welcome02 from 'containers/auth/welcome/Welcome02'
 
 
 const App = () => {
@@ -15,9 +17,9 @@ const App = () => {
     <GestureHandlerRootView>
       <StatusBar translucent backgroundColor={'rgba(0,0,0,0)'}/>
       <View flex> 
-          
           {/* <Signin/> */}
-          <CameraApp/>
+          {/* <CameraApp/> */}
+          <Setting/>
         </View>
     </GestureHandlerRootView>
    
