d6da914dc9e9b61f13a5c30e2fe814f960f3ffe3
phuchoang85 <114634047+phuchoang85@users.noreply.github.com>
2024-06-12 22:31:51 +0700

    new change ben app

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { StatusBar, StyleSheet } from 'react-native'
 import React from 'react'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import DevNavigation from 'containers/navigations/DevNavigation'
 import { Provider } from 'react-redux'
 import { store, persistor } from 'src/store/store'
@@ -11,12 +10,10 @@ const App = () => {
   return (
     <Provider store={store}>
       <I18nProvider>
-        <PersistGate loading={null} persistor={persistor}>
-          <GestureHandlerRootView>
+        <PersistGate loading={null} persistor={persistor}>  
             <StatusBar translucent backgroundColor={'rgba(0,0,0,0)'} />
             {/* <DevNavigation /> */}
             <MainNavigation />
-          </GestureHandlerRootView>
         </PersistGate>
       </I18nProvider>
     </Provider>
