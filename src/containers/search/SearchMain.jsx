4a523006ced771cf602e43f5e19efef529b9e49b
nguyenquyynh <nguyenxuanquynh1812nc1@gmail.com>
2024-06-22 17:37:57 +0700

    search (History)

--- src/containers/search/SearchMain.jsx
+++ src/containers/search/SearchMain.jsx
@@ -6,15 +6,25 @@ import IconApp from 'components/IconApp'
 import LinearGradient from 'react-native-linear-gradient'
 import { t } from 'lang'
 import { useNavigation } from '@react-navigation/native'
+import HistoryList from './HistoryList'
+import SearchList from './SearchList'
+import { useDispatch } from 'react-redux'
+import { history_addsearch } from 'reducers/search'
 
 const SearchMain = () => {
     const navigation = useNavigation()
+    const dispatch = useDispatch()
     const [keyword, setKeyword] = useState('')
     const [data_search, setData_search] = useState(false)
     var windowWidth = Dimensions.get('window').width;
 
     const gotoScreen = (srceen, props) => {
         navigation.navigate(srceen, props)
+    }
+    const hanlderSearch = () => {
+        if (keyword.trim().length > 0) {
+            dispatch(history_addsearch(keyword))
+        }
     }
     const customerHeader = () => {
         return (
@@ -27,27 +37,28 @@ const SearchMain = () => {
                 />
                 <View absF bottom paddingB-xi>
                     <View centerV row paddingH-xvi>
-                        <Pressable onPress={()=> {navigation.goBack()}}>
+                        <Pressable onPress={() => { navigation.goBack() }}>
                             <IconApp assetName={"back"} size={windowWidth / 22} />
                         </Pressable>
-                        <View bg-white flex br20 marginL-xvi paddingL-x style={styles.searchinput}>
-                            <TextInput placeholder={t("app.search")} value={keyword} onChangeText={value => setKeyword(value)}/>
+                        <View bg-white row flex br20 marginL-xvi centerV paddingL-x style={styles.searchinput}>
+                            <View flex>
+                                <TextInput placeholder={t("app.search")} value={keyword} onChangeText={value => setKeyword(value)} />
+                            </View>
+                            <View paddingH-x>
+                                <Pressable onPress={hanlderSearch}>
+                                    <IconApp assetName={"search"} size={20} />
+                                </Pressable>
+                            </View>
                         </View>
                     </View>
                 </View>
             </View>
         )
     }
-    const renderDataSearch = () => {
-        
-    }
-    const renderHistory = () => {
-
-    }
     return (
         <Wapper gadient header customheader={customerHeader}>
             {
-                data_search ? renderDataSearch : renderHistory  
+                data_search ? <SearchList /> : <HistoryList keyword={keyword} setKeyword={setKeyword} />
             }
         </Wapper>
     )
--- src/store/store.js
+++ src/store/store.js
@@ -3,14 +3,16 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { persistReducer, persistStore } from 'redux-persist'
 import auth from 'src/store/reducers/auth'
 import setting from 'src/store/reducers/setting'
+import search from 'src/store/reducers/search'
 const persistConfig = {
     key: 'store',
     storage: AsyncStorage,
-    whitelist: ['auth', 'setting'],
+    whitelist: ['auth', 'setting', 'search'],
 }
 const rootReducer = combineReducers({
     auth: auth,
-    setting: setting
+    setting: setting,
+    search : search,
 })
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
