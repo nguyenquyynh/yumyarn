b982dda80e6a82ba673115b7e85271554b5ec79e
Nguyễn Xuân Quỳnh <nguyenxuanquynh1812nc1@gmail.com>
2024-10-26 20:43:38 +0700

    Ui List history

--- src/services/AxiosInstance.js
+++ src/services/AxiosInstance.js
@@ -5,7 +5,7 @@ import { store } from 'store/store';
 
 const AxiosInstance = (token = '', contentType = 'application/json') => {
   const axiosInstance = axios.create({
-    baseURL: 'http://192.168.1.26:3001/',
+    baseURL: process.env.BASEAPI_URL,
   });
 
   axiosInstance.interceptors.request.use(
