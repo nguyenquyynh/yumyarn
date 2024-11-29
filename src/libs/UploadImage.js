ac69f398e5bf51dd07a5995682eee9accacfcaf5
phuchoang85 <114634047+phuchoang85@users.noreply.github.com>
2024-11-29 20:30:50 +0700

    update

--- src/libs/UploadImage.js
+++ src/libs/UploadImage.js
@@ -3,7 +3,7 @@ export const Upload = async (uri, type, name) => {
     data.append('file', { uri, type, name });
     data.append('upload_preset', 'x1r3euwt');
     
-    const response = await fetch(`https://api.cloudinary.com/v1_1/ddgmnqwtk/upload`, {
+    const response = await fetch(`https://api.cloudinary.com/v1_1/dggd2eydr/upload`, {
         method: "POST",
         headers: {
             "Accept": "application/json",
